30d2df10772f47bec90fc2fd18886e7ad417559a
LizaSano1 <145109365+LizaSano1@users.noreply.github.com>
2023-12-07 22:55:38 +0100

    pp

--- projektpob1.py
+++ projektpob1.py
@@ -1,19 +1,22 @@
-import pytest
-from flask import Response
+from flask import Flask, request, jsonify
 
 app = Flask(__name__)
+
 users = []
 
+@app.route('/', methods=['GET'])
+def hello():
+    return 'Hello, this is an HTTP/1.1 server!'
 
 @app.route('/users', methods=['GET'])
 def get_users():
-    return jsonify(users), 200
+    return jsonify(users)
 
+@app.route('/users', methods=['POST'])
+def create_user():
+    data = request.json
+    users.append(data)
+    return jsonify({"message": "User created successfully"}), 201
 
-@app.route('/users/<int:user_id>', methods=['GET'])
-def get_user(user_id):
-    user = next((user for user in users if user['id'] == user_id), None)
-    if user:
-        return jsonify(user), 200
-    else:
-        return jsonify({'error': 'User not found'}), 404
+if __name__ == '__main__':
+    app.run('127.0.0.1', 8080, debug=True)
