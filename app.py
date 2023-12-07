bdb20cfb776c5a58b69d26993420dca503c8383f
LizaSano1 <145109365+LizaSano1@users.noreply.github.com>
2023-12-07 23:10:40 +0100

    cssc

--- app.py
+++ app.py
@@ -25,7 +25,7 @@ def get_user(user_id):
     if user:
         return jsonify(user)
     else:
-        return jsonify({"error": f"User with id {user_id} not found"}), 404
+        return jsonify({"error": f"User with id {user_id} not found"}), 200
 
 @app.route('/users', methods=['POST'])
 def create_user():
@@ -44,11 +44,34 @@ def create_user():
 def update_user(user_id):
     user = get_user_by_id(user_id)
     if not user:
-        return jsonify({"error": f"User with id {user_id} not found"}), 404
+        return jsonify({"error": f"User with id {user_id} not found"}), 400
 
     data = request.json
     if 'name' in data:
         user['name'] = data['name']
     if 'lastname' in data:
         user['lastname'] = data['lastname']
-    return "", 204
+    return jsonify({"message": "User updated successfully"}), 204
+
+@app.route('/users/<int:user_id>', methods=['PUT'])
+def replace_user(user_id):
+    user = get_user_by_id(user_id)
+    if not user:
+        return jsonify({"error": f"User with id {user_id} not found"}), 400
+
+    data = request.json
+    user['name'] = data.get("name", "")
+    user['lastname'] = data.get("lastname", "")
+    return jsonify({"message": "User replaced successfully"}), 204
+
+@app.route('/users/<int:user_id>', methods=['DELETE'])
+def delete_user(user_id):
+    user = get_user_by_id(user_id)
+    if not user:
+        return jsonify({"error": f"User with id {user_id} not found"}), 400
+
+    users.remove(user)
+    return jsonify({"message": "User deleted successfully"}), 204
+
+if __name__ == '__main__':
+    app.run('127.0.0.1', 8080, debug=True)
